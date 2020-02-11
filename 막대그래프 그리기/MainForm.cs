1492522efb99263dc3899e145196db881b420de1
asdf-2345 <kiaaa741@gmail.com>
2020-02-11 14:21:22 +0900

    그래프가 패널을 넘어갈때 줄여줌

--- 막대그래프 그리기/MainForm.cs
+++ 막대그래프 그리기/MainForm.cs
@@ -55,21 +55,51 @@ namespace 막대그래프_그리기
 				GraphTopics = new int[a];
 				GraphTopicsEnter(a);
 			}
-			catch{
-				MessageBox.Show("다시 입력해주세요.");
+			catch(Exception ex){
+				MessageBox.Show(ex.ToString());
 				return;
 			}
 		}
 		
 		bool drawPossibility = false;
 		void GraphTopicsEnter(int length){
+			try{
+			int max = 0, min = int.MaxValue;
 			for(int a = 1; a <= length; a++){
 				Form1 form1 = new Form1();
 				form1.num = a;
 				form1.ShowDialog();
 				GraphTopics[a-1] = form1.Size;
+				if(min > GraphTopics[a-1]){
+					min = GraphTopics[a-1];
+				}
+				if(max < GraphTopics[a-1]){
+					max = GraphTopics[a-1];
+				}
 				Console.WriteLine(GraphTopics[a-1]);
 			}
+			Console.WriteLine(max + " " + min);
+			Console.WriteLine();
+			Console.WriteLine(panel1.Width);
+			if(max > panel1.Width){
+				Console.WriteLine(panel1.Width);
+				Console.WriteLine(max);
+				Console.WriteLine(panel1.Width / max);
+				double multiplicationNumber = (double)panel1.Width / (double)max;
+				Console.WriteLine(multiplicationNumber);
+				for(int a = 0; a < GraphTopics.Length; a++){
+					GraphTopics[a] = (int)Math.Round(GraphTopics[a] * multiplicationNumber - 1);
+					Console.WriteLine(GraphTopics[a]);
+				}
+			}
+			else if(min < 51){
+				int multiplicationNumber = 51 / min;
+				
+				if(max * multiplicationNumber + 5 < panel1.Width){
+					
+				}
+			}
+			
 			drawPossibility = true;
 			
 			Pen p = new Pen(Color.Black, 1);
@@ -78,6 +108,10 @@ namespace 막대그래프_그리기
 			for(int a = 0; a < GraphTopics.Length; a++){
 				drawRectangle(p, g, 10 + a * 30, GraphTopics[a]);
 			}
+		}
+		catch(Exception ex){
+				MessageBox.Show(ex.ToString());
+		}
 		}
 	}
 }
