af2116f08d61ff878f6ffde6640c57d86ab97c61
asdf-2345 <kiaaa741@gmail.com>
2020-02-11 12:07:08 +0900

    업로드

---
+++ 막대그래프 그리기/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: asdf-2345
+ * Date: 2020-02-08
+ * Time: 오후 2:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace 막대그래프_그리기
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.panel1.Location = new System.Drawing.Point(12, 164);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(893, 430);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1Paint);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(917, 606);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "막대그래프 그리기";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ 막대그래프 그리기/MainForm.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: asdf-2345
+ * Date: 2020-02-08
+ * Time: 오후 2:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace 막대그래프_그리기
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void drawRectangle(Pen p, Graphics g){
+			int y = 10;
+			int x = 50;
+			Rectangle rec = new Rectangle(0, y, x, 20);
+            g.DrawRectangle(p, rec);
+		}
+		
+		void drawLine(Pen p, Graphics g){
+			int y = 10;
+			int x = 50;
+			Point point1 = new Point(0, 0);
+			Point point2 = new Point(0, panel1.Height);
+			g.DrawLine(p, point1, point2);
+		}
+		
+		void Panel1Paint(object sender, PaintEventArgs e)
+		{
+			Pen p = new Pen(Color.Black, 1);
+			Graphics g = this.panel1.CreateGraphics();
+			drawLine(p, g);
+			drawRectangle(p, g);
+		}
+	}
+}
